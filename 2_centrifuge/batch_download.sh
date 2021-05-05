f7147f084a030ec3836f343a97df2870c6b67b48
Vidya Vuruputoor <vidyavuru@gmail.com>
2021-05-05 16:53:28 -0400

    Update batch_download.sh

--- 2_centrifuge/batch_download.sh
+++ 2_centrifuge/batch_download.sh
@@ -7,11 +7,17 @@
 #SBATCH --qos=general
 #SBATCH --mail-type=END
 #SBATCH --mem=100G
-#SBATCH --mail-user=vidya.vuruputoor@uconn.edu
+#SBATCH --mail-user=
 #SBATCH -o centrifuge_download_%j.out
 #SBATCH -e centrifuge_download_%j.err
 
+##load relevant modules and versions
 module load centrifuge/1.0.4-beta
 module load blast/2.7.1
+
+## centrifuge-download downloads the relevant taxonomy files 
+  # -o output folder
+  # -m low complexity regions are masked
+  # -d databases
 centrifuge-download -o taxonomy taxonomy
 centrifuge-download -o library -m -d "archaea,bacteria,viral" refseq > seqid2taxid.map
